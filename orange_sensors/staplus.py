130cef57a1d50f8b02e91d79a55b992f4d7b1012
José P. Gómez B. S. <jpablo.gomezb@hotmail.com>
2024-05-02 14:32:04 +0100

    Updated Info text output after fetching data

--- orange_sensors/staplus.py
+++ orange_sensors/staplus.py
@@ -1,12 +1,13 @@
+from datetime import datetime, date, time
+import Orange.data
 from orangewidget.widget import OWBaseWidget, Output
 from orangewidget.settings import Setting
 from orangewidget import gui
 from orangewidget.utils.widgetpreview import WidgetPreview
-import Orange.data
 from Orange.data.pandas_compat import table_from_frame
-
 import secd_staplus_client as STAplus
 from pandas import DataFrame
+
 
 # Fixed stations
 class STAplusWidget(OWBaseWidget):
@@ -24,10 +25,10 @@ class STAplusWidget(OWBaseWidget):
     priority = 3
 
     # Basic (convenience) GUI definition:
-    #   a simple 'single column' GUI layout
+    # a simple 'single column' GUI layout
     want_main_area = False
 
-    #   with a fixed non resizable geometry.
+    # with a fixed non-resizable geometry.
     resizing_enabled = True
 
     # Defining settings
@@ -37,21 +38,23 @@ class STAplusWidget(OWBaseWidget):
 
     # Widget's outputs; here, a single output named "Observations", of type Table
     class Outputs:
-        observationsTable = Output("Things", Orange.data.Table, auto_summary=False)
+        observationsTable = Output(
+            "Things", Orange.data.Table, auto_summary=False)
 
     def __init__(self):
         # use the init method from the class OWBaseWidget
         super().__init__()
 
         # info area
-        info = gui.widgetBox(self.controlArea, "Info")
+        info = gui.widgetBox(self.controlArea, "Info.")
 
         # Info banners
         self.infoa = gui.widgetLabel(info, 'No data fetched yet.')
 
         gui.separator(self.controlArea)
 
-        self.searchBox = gui.widgetBox(self.controlArea, "URL")
+        self.searchBox = gui.widgetBox(
+            self.controlArea, "STA server URL")
         self.url_line = gui.lineEdit(
             self.searchBox,
             self,
@@ -61,7 +64,8 @@ class STAplusWidget(OWBaseWidget):
             controlWidth=200
             )
 
-        self.searchBox = gui.widgetBox(self.controlArea, "Get data from a Thing")
+        self.searchBox = gui.widgetBox(
+            self.controlArea, "Get data from a Thing")
         self.thing_line = gui.lineEdit(
             self.searchBox,
             self,
@@ -75,8 +79,14 @@ class STAplusWidget(OWBaseWidget):
         gui.separator(self.controlArea)
 
         # commit area
-        self.commitBox = gui.widgetBox(self.controlArea, "", spacing=2)
-        gui.button(self.commitBox, self, "Get the Thing!", callback=self.commit)
+        self.commitBox = gui.widgetBox(
+            self.controlArea, "", spacing=2)
+        gui.button(
+            self.commitBox,
+            self,
+            "Get the Thing data!",
+            callback=self.commit
+        )
 
     def commit(self):
         # TODO Improve progress bar based on datastreams...
@@ -92,40 +102,60 @@ class STAplusWidget(OWBaseWidget):
                 datastreams = thing.get_datastreams().query().list()
                 result_df = DataFrame()
 
+                datastreams_name = list()
                 for datastream in datastreams:
                     data = dict()
-                    # print (f'Datastream ID: {datastream.id}')
+                    # print(f'Datastream ID: {datastream.id}')
+                    datastreams_name.append(datastream.name)
                     observations = datastream.get_observations().query().list()
-                    observed_property = datastream.get_observed_property().query().item()
-                    # print (observed_property.name, observed_property.id, observed_property.description)
+                    observed_property = (
+                        datastream.get_observed_property().query().item())
+                    # print(observed_property.name, observed_property.id,
+                    #       observed_property.description)
                     timestamps = list()
                     results = list()
                     # TODO Can this be done in an better way with the actual client?
-                    for b in observations:
+                    for obs in observations:
+                        # print(obs.phenomenon_time,
+                        #       obs.result, datastream.unit_of_measurement.name)
                         # TODO Small hack for UX
-                        timestamps.append(b.phenomenon_time)
-                        results.append(b.result)
-                        #print (b.phenomenon_time, b.result, datastream.unit_of_measurement.name)
-                    data['TIME'] = timestamps
-                    data[f'{observed_property.name} ({datastream.unit_of_measurement.name})'] = results
+                        timestamps.append(obs.phenomenon_time)
+                        results.append(obs.result)
+
+                    data['DATE HOUR'] = timestamps
+                    data[(f'{observed_property.name} '
+                          f'({datastream.unit_of_measurement.name})')] = results
                     # TODO Improve progress bar
                     progress.advance()
 
-                    df = DataFrame.from_dict(data).set_index('TIME')
+                    df = DataFrame.from_dict(data).set_index('DATE HOUR')
                     result_df = result_df.combine_first(df)
 
                 table = table_from_frame(result_df)
 
                 progress.advance()
                 self.Outputs.observationsTable.send(table)
-                self.infoa.setText(f'Thing Observations {self.thing_id} gathered')
+                self.infoa.setText(
+                    f'Observations for Thing ID {self.thing_id} gathered.')
+                datastreams_formatted = "".join(
+                    f"<li>{stream}</li>" for stream in datastreams_name)
+                self.infoa.setText(
+                    f"""<p>Observations for Thing ID. 
+                    {self.thing_id} gathered<br><br>
+                    Datastreams found:
+                    <ul>{datastreams_formatted}</ul></p>
+                    <p>You can connect the data to a table now!<br></p>"""
+                )
+
                 self.info.set_output_summary(1)
 
             else:
-                self.infoa.setText(f'Device {self.thing_id} not found. Check again')
+                self.infoa.setText(
+                    f'Device {self.thing_id} not found. Check again')
                 self.info.set_output_summary(self.info.NoOutput)
 
         progress.finish()
+
 
 # For developer purpose, allow running the widget directly with python
 if __name__ == "__main__":
